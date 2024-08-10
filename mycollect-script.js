e0e9b08be5e66b8f3fd8147944fc11fafa6632f3
Loris Pandaru <loris.buchelet@gmail.com>
2024-08-10 19:14:43 +0200

    header fixed on every page

--- mycollect-script.js
+++ mycollect-script.js
@@ -35,30 +35,37 @@ function loadCollection(collection) {
     Promise.all(fetchPromises)
         .then(pokemons => {
             // pokemons array will be in the same order as the collection array
-            pokemons.forEach(myShinyPokemon => {
-                const pokeId = myShinyPokemon.pokedex_id;
+            pokemons.forEach(pokemon => {
+                const pokeId = pokemon.pokedex_id;
                 let currentCount = parseInt(localStorage.getItem(pokeId)) || 0; // Parse as integer
 
                 // Create list item for the Pokémon
                 const listItem = document.createElement('li');
                 listItem.innerHTML = `
-                    <div class="sprite-bg">
-                        <div class="middle-round-pkball"></div>
-                        <div class="middle-sep-pkball"></div>
-                        <img class="pkmn-sprite" src="${myShinyPokemon.sprites.shiny || myShinyPokemon.sprites.regular}" alt="${myShinyPokemon.name.fr}">
-                    </div>
-                    <br>
-                    <b># ${pokeId}</b><br>
-                    <b>${myShinyPokemon.name.en}</b> <i>FR: ${myShinyPokemon.name.fr}</i><br>
-                    <div>
-                        <img src="${myShinyPokemon.types[0].image}" alt="type" class="pkmn-type">
-                        ${myShinyPokemon.types[1] ? `<img src="${myShinyPokemon.types[1].image}" alt="type" class="pkmn-type"><br>` : ''}
-                    </div>
-                    <div class="pkm-collection">
-                        <button class="addPokemon" id="addPokemon${pokeId}"><i class="bi bi-plus-circle"></i></button>
-                        <span class="countPokemon" id="countPokemon${pokeId}">x${currentCount}</span>
-                        <button class="deletePokemon" id="deletePokemon${pokeId}"><i class="bi bi-dash-circle"></i></button>
-                    </div>
+                    <div class="pkmn-background ${currentCount > 0 ? 'sprite-bg-caught' : 'sprite-bg'}">
+            <div class="middle-round-pkball"></div>
+            <div class="middle-sep-pkball"></div>
+            <img class="pkmn-sprite" src="${pokemon.sprites.shiny || pokemon.sprites.regular}" alt="${pokemon.name.fr}">
+        </div>
+        <div class="small-screen">
+            <div class="d-block d-md-flex flex-md-column pkm-identity">
+                <b># ${pokemon['pokedex_id']}</b>
+                <b>${pokemon['name']['en']}</b>
+                <i>FR: ${pokemon['name']['fr']}</i>
+            </div>
+            <div>
+                <img src="${pokemon.types[0].image}" alt="type" class="pkmn-type">
+                ${pokemon.types[1] ? `<img src="${pokemon.types[1].image}" alt="type" class="pkmn-type"><br>` : ''}
+            </div>
+            <div class="pkm-collection mb-2">
+                <button class="addPokemon" id="addPokemon${pokemon['pokedex_id']}"><i class="bi bi-plus-circle"></i></button>
+                <span class="countPokemon" id="countPokemon${pokemon['pokedex_id']}">x${currentCount}</span>
+                <button class="deletePokemon" id="deletePokemon${pokemon['pokedex_id']}"><i class="bi bi-dash-circle"></i></button>
+            </div>
+        </div>
+        <div>
+            <a href="/pokedex?num=${pokemon['pokedex_id']}" class="btn accesspage"><span class="d-none d-md-inline">See more</span><span class="d-inline d-md-none""><i class="bi bi-eye"></i></span></a>
+        </div>
                 `;
                 myCollectionSection.appendChild(listItem);
 
